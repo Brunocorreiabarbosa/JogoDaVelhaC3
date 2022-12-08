701939d33fd89eeb27f3d5764f2dad18d5bbfc7e
Bruno Barbosa <42818756+Brunocorreiabarbosa@users.noreply.github.com>
2022-12-08 15:28:27 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Board from './components/Board.jsx';
+import Game from './components/Game.jsx';
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
